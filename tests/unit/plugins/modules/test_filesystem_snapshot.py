0f1862642a0c6fadd72a682502110a4dd90eff4a
sfulmer <sfulmer@redhat.com>
2026-05-17 12:27:29 -0400

    fix: reformat after boilerplate addition

--- tests/unit/plugins/modules/test_filesystem_snapshot.py
+++ tests/unit/plugins/modules/test_filesystem_snapshot.py
@@ -1,6 +1,7 @@
 """Unit tests for stevefulme1.lambdalabs.filesystem_snapshot module."""
 
 from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 from unittest.mock import MagicMock
 
