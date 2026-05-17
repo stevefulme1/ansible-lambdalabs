a9f6cf345297311ffd7c2e10f14c4f2b81dbc3c5
sfulmer <sfulmer@redhat.com>
2026-05-16 22:21:48 -0400

    feat: add distributed training and job scheduler modules
    Add 4 new modules:
    - distributed_training: configure distributed training jobs
    - distributed_training_info: retrieve training job info
    - job_scheduler: schedule/manage GPU jobs
    - job_scheduler_info: retrieve scheduled job info

---
+++ plugins/modules/distributed_training_info.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2026, Steve Fulmer
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+"""List or get details about Lambda Labs distributed training jobs."""
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: distributed_training_info
+short_description: Get Lambda Labs distributed training job info
+description:
+  - Retrieve information about distributed training jobs on Lambda Labs.
+version_added: "1.0.0"
+extends_documentation_fragment:
+  - stevefulme1.lambdalabs.lambdalabs
+options:
+  job_id:
+    description: Specific job ID to query. Omit to list all.
+    type: str
+author:
+  - Steve Fulmer (@stevefulme1)
+"""
+
+EXAMPLES = r"""
+- name: List all training jobs
+  stevefulme1.lambdalabs.distributed_training_info:
+    api_key: "{{ lambda_api_key }}"
+  register: result
+
+- name: Get a specific training job
+  stevefulme1.lambdalabs.distributed_training_info:
+    api_key: "{{ lambda_api_key }}"
+    job_id: job-abc123
+"""
+
+RETURN = r"""
+training_jobs:
+  description: List of training job details.
+  type: list
+  returned: always
+  elements: dict
+"""
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.stevefulme1.lambdalabs.plugins.module_utils.lambda_client import (
+    LambdaClient,
+    LambdaError,
+    lambda_argument_spec,
+)
+
+
+def main():
+    spec = lambda_argument_spec()
+    spec.update(job_id=dict(type="str"))
+    module = AnsibleModule(argument_spec=spec, supports_check_mode=True)
+
+    client = LambdaClient(module.params["api_key"], module.params["timeout"])
+
+    try:
+        jid = module.params.get("job_id")
+        if jid:
+            result = client.get("instances/{0}".format(jid))
+            jobs = [result.get("data", {})]
+        else:
+            result = client.get("instances")
+            jobs = result.get("data", [])
+        module.exit_json(changed=False, training_jobs=jobs)
+    except LambdaError as exc:
+        module.fail_json(msg=str(exc))
+
+
+if __name__ == "__main__":
+    main()
